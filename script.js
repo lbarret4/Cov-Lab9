809dbe4b1aeca2338edd29e5b222d5872714c1ed
lbarret4 <llewellynbar4@gmail.com>
2018-08-20 18:00:11 -0400

    Modified script.js to add button to index file

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('Add Square');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+
+    
+});
+
+function addSquare(){
+    let squareDiv = documents
+    button.addEventListener('click', 
+}
+
+
+
