787dc479004a68442e30df69e10b5501b931b8a9
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 15:17:57 -0400

    Added styles to center text and update fuction to assign ids to square

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ window.addEventListener('DOMContentLoaded', function () {
     let button = document.createElement('button');
     let btnText = document.createTextNode('Add Square');
     button.appendChild(btnText);
-    button.setAttribute('id','addSqrBtn');
+    button.setAttribute('id', 'addSqrBtn');
     document.body.appendChild(button);
 
-    button.addEventListener('click',addSquare)
+   button.addEventListener('click', addSquare)
 });
 
-function addSquare(){
+function addSquare() {
+
     let squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class','sqr-div');
+    squareDiv.setAttribute('class', 'sqr-div');
+    let numSqr = document.getElementsByClassName('sqr-div').length;
+    let squareDivText = document.createTextNode(`${(numSqr + 1)}`);
+    squareDiv.appendChild(squareDivText);
+    squareDiv.setAttribute('id', `${(numSqr + 1)}`);
     document.body.appendChild(squareDiv);
+
+
 }
-
-
-
