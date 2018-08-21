55432e690081d595251ccee3f50887a661b050be
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 16:09:28 -0400

    Add event to change color of square with double click and setColor

--- script.js
+++ script.js
@@ -5,29 +5,34 @@ window.addEventListener('DOMContentLoaded', function () {
     button.setAttribute('id', 'addSqrBtn');
     document.body.appendChild(button);
 
-   button.addEventListener('click', addSquare)
+    button.addEventListener('click', addSquare)
 });
 
-function addSquare(){
+function addSquare() {
 
     let squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class','sqr-div');
-    let numSqr= document.getElementsByClassName('sqr-div').length;
-    let squareDivText = document.createTextNode(`${(numSqr+1)}`);
+    squareDiv.setAttribute('class', 'sqr-div');
+    let numSqr = document.getElementsByClassName('sqr-div').length;
+    let squareDivText = document.createTextNode(`${(numSqr + 1)}`);
     squareDiv.appendChild(squareDivText);
-     squareDiv.addEventListener('mouseover',idSqrShow);
-      squareDiv.addEventListener('mouseleave',idSqrShow);
-      
-    squareDiv.setAttribute('id',`${(numSqr+1)}`);
+    squareDiv.addEventListener('mouseover', idSqrShow);
+    squareDiv.addEventListener('mouseleave', idSqrShow);
+    squareDiv.addEventListener('dblclick', getColor);
+    squareDiv.setAttribute('id', `${(numSqr + 1)}`);
     document.body.appendChild(squareDiv);
-  }
-    
-    function idSqrShow (e){
-      if(e.type === 'mouseover'){
-         e.target.style.color="white";
-      }else if( e.type === 'mouseleave'){
-         e.target.style.color="black";
-      }
-     
-      
+}
+
+function idSqrShow(e) {
+    if (e.type === 'mouseover') {
+        e.target.style.color = "white";
+    } else if (e.type === 'mouseleave') {
+        e.target.style.color = "black";
     }
+
+    function setColor(e) {
+        let [red, green, blue] = [Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255)];
+        e.target.style.background = `rgb(${red},${green},${blue})`;
+    }
+
+
+}
