041b824df4b9879088fc26c65070991a73e4f7e4
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 13:10:18 -0400

    Updated addSquare function to add square divs

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ window.addEventListener('DOMContentLoaded', function () {
     let button = document.createElement('button');
     let btnText = document.createTextNode('Add Square');
     button.appendChild(btnText);
+    button.setAttribute('id','addSqrBtn');
     document.body.appendChild(button);
 
     button.addEventListener('click',addSquare)
 });
 
 function addSquare(){
-    let squareDiv ;
-    
+    let squareDiv = document.createElement('div');
+    squareDiv.setAttribute('class','sqr-div');
+    document.body.appendChild(squareDiv);
 }
 
 
