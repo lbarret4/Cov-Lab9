1416665bb5899f798dc21781177cca827aedead6
lbarret4 <llewellynbar4@gmail.com>
2018-08-21 21:32:53 -0400

    Completed  fully functional DOM DOM exercise

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', function () {
     button.setAttribute('id', 'addSqrBtn');
     document.body.appendChild(button);
     button.addEventListener('click', addSquare)
-    
+
 });
 
 function addSquare() {
@@ -18,7 +18,7 @@ function addSquare() {
     squareDiv.addEventListener('mouseover', idSqrShow);
     squareDiv.addEventListener('mouseleave', idSqrShow);
     squareDiv.addEventListener('click', setColor);
-    squareDiv.addEventListener('dblclick',rmSquare);
+    squareDiv.addEventListener('dblclick', rmSquare);
     squareDiv.setAttribute('id', `${(numSqr + 1)}`);
     document.body.appendChild(squareDiv);
 }
@@ -30,25 +30,25 @@ function idSqrShow(e) {
         e.target.style.color = `${e.target.style.backgroundColor}`
     }
 }
-    function setColor(e) {
-        let [red, green, blue] = [Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255)];
-        e.target.style.background = `rgb(${red},${green},${blue})`;
+function setColor(e) {
+    let [red, green, blue] = [Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255)];
+    e.target.style.background = `rgb(${red},${green},${blue})`;
+}
+
+function rmSquare(e) {
+    let id = e.target.getAttribute('id');
+    let adjacentSqr;
+
+
+    if (id % 2 === 0) {
+        adjacentSqr = e.target.nextSibling;
+        (adjacentSqr !== null) ? adjacentSqr.remove() : window.alert('Unable to remove next square')
+    } else {
+        adjacentSqr = e.target.previousSibling;
+        (adjacentSqr !== null && adjacentSqr.nodeName !== 'BUTTON') ? adjacentSqr.remove() : window.alert('Unable to remove previous square');
     }
 
-    function rmSquare(e){
-        let id = e.target.getAttribute('id');
-        let adjacentSqr; 
-  
-        
-      if(id % 2 === 0){
-         adjacentSqr = e.target.nextSibling;
-        ( adjacentSqr !== null) ?   adjacentSqr.remove() : window.alert('Unable to remove next square')  
-      }else{
-          adjacentSqr = e.target.previousSibling;
-        ( adjacentSqr !== null && adjacentSqr.nodeName !== 'BUTTON') ?   adjacentSqr.remove() : window.alert('Unable to remove previous square') ;
-      }
-      
-    }
+}
 
 
 
